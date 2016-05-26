94800357493ec528aa194cd0a51b88af62c712f5
jtcode42 <tschetterjordan@gmail.com>
2016-05-26 12:51:01 -0500

    Create bottlesofcoke.js

---
+++ bottlesofcoke.js
@@ -0,0 +1,6 @@
+var bottles=100;
+
+for(i=100; i > 0; i--){
+  console.log(bottles + " bottles of coke on the wall, " + bottles + " of coke. Take one down, pass it around, " + bottles-1 " bottles of coke on the wall");
+  bottles=bottles-1;
+};
